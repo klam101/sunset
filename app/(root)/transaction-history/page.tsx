80549975bb57e5f8af6599f061bef44757d6799f
LumpyTacos <kevin.lam@stemrocks.org>
2026-06-24 00:49:41 -0400

    Implemented base layout: - Sidebars - Navigation destinations - Mobile compatability
    Changed some globals.css variables to make styling better

---
+++ app/(root)/transaction-history/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TransctionHistory = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default TransctionHistory
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -17,31 +17,35 @@ const MobileNav = ({ user }: MobileNavProps) => {
           <Image src="/icons/hamburger.svg" width={30} height={30} alt="menu" className="cursor-pointer" />
         </SheetTrigger>
         <SheetContent side="left" className="border-none bg-white">
-          <Link href="/" className=" cursor-pointer flex items-center gap-1 !px-4">
+          <Link href="/" className="cursor-pointer flex items-center gap-1 !px-4">
             <Image src="/icons/logo.svg" width={34} height={34} alt="Sunset Logo" className="size-[3rem]" />
             <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">Sunset</h1>
           </Link>
           <div className="mobilenav-sheet">
             <SheetClose asChild>
-              <nav className="flex h-full flex-col gap-6 pt-16 text-white">
+              <nav className="flex h-full flex-col gap-6 text-white">
                 {sidebarLinks.map((item) => {
                   const isActive = pathName === item.route || pathName.startsWith(`${item.route}/`)
 
                   return (
-                    <SheetClose asChild key={item.route}>
-                      <Link href={item.route} key={item.label} className={cn("mobilenav-sheet_close w-full", {"bg-bank-gradient": isActive})}>
-                        <div className="relative size-6">
+                    <SheetClose asChild key={item.route} className="text-16"> {/* <p> inherits className attributes from this tag, therefore adding the "text-16" changes the text size */}
+                      <Link href={item.route} key={item.label} className={cn("mobilenav-sheet_close", {"bg-bank-gradient": isActive})}>
+                        <div className="flex items-center gap-3 p-4 relative size-6">
                           <Image src={item.imgURL} alt={item.label} fill className={cn({"brightness-[3] invert-0": isActive})} />
                         </div>
-                        <p className={cn("text-24 font-semibold text-black-2", {"!text-white" : isActive})}>
+                        <p className={cn("text-24 font-semibold text-black-2", {"text-white" : isActive})}> {/* Why does text-24 not work in cn() */}
                           {item.label}
                         </p>
                       </Link>
                     </SheetClose>
                   )
                 })}
+
+                USER
               </nav>
             </SheetClose>
+
+            FOOTER
           </div>
         </SheetContent>
       </Sheet>
