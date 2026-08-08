3e70f3cbd1e4b3dbbd48ec15086ae56d17ae4d1e
LumpyTacos <kevin.lam@stemrocks.org>
2026-08-08 13:52:22 -0400

    added layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
       {children}
       <div className="auth-asset">
         <div>
-          <Image src="/icons/auth-image.svg" width={500} height={500} alt="Auth Image" />
+          <Image src="/images/demo.png" width={700} height={700} alt="Auth Image" />
         </div>
       </div>
     </main>
